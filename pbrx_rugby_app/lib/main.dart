3eeb604420b3f60d8d8db0724244de407ed585df
Megan Miles <home.megan.miles@gmail.com>
2025-01-27 15:59:47 +0000

    playing around with dart file to understand concepts

--- pbrx_rugby_app/lib/main.dart
+++ pbrx_rugby_app/lib/main.dart
@@ -14,10 +14,10 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'PBRX Rugby App',
         theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          useMaterial3: true, //this is changing the theme e.g. button looks like
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 34, 170, 255)), //this is colour theme
         ),
         home: MyHomePage(),
       ),
@@ -25,10 +25,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//class that is generating the random words using the a package installed at the top
+//this is where functionality goes, varibales and methods/functions will go here.
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+  var current = WordPair.random(); //creates variable that can be used later 
+
+  //method that updated current to new random pair
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
+
+//this is where the app is kinda building what goes on to the UI in widgets
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -37,8 +47,17 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
+          Text('A random idea, by Megan Miles, again:'), //simple text
+          Text(appState.current.asLowerCase), //this is the random word pair, current is taken from the class above 
+
+          ElevatedButton(
+            onPressed: () {
+              //print('button pressed!'); //prints on console
+              //when button is pressed the appState runs the getNext method
+              appState.getNext();
+            },
+            child: Text('Next'), //text of button, this could be ANY widget in Flutter
+          ),
         ],
       ),
     );
